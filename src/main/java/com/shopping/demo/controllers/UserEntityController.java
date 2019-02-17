4a7e50e65e7a73f71b8e94169025642bef947c70
Master_Cloud <lsclike2016@hotmail.com>
2019-02-17 19:06:28 +1100

    adding paging and sorting

--- src/main/java/com/shopping/demo/controllers/UserEntityController.java
+++ src/main/java/com/shopping/demo/controllers/UserEntityController.java
@@ -3,16 +3,18 @@ package com.shopping.demo.controllers;
 import com.shopping.demo.data.entity.UserEntity;
 import com.shopping.demo.data.repository.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping(path="api/users")
 public class UserEntityController {
+    PageRequest page = PageRequest.of(0, 1, Sort.by(""));
     private UserEntityRepository userEntityRepository;
 
     @Autowired
@@ -21,7 +23,7 @@ public class UserEntityController {
     }
 
     @GetMapping("/")
-    public @ResponseBody List<UserEntity> allUser(){
+    public List<UserEntity> allUser(){
         return userEntityRepository.findAll();
     }
 }
--- src/main/java/com/shopping/demo/data/test.java
+++
@@ -1,5 +0,0 @@
-package com.shopping.demo.data;
-
-public class test {
-
-}
